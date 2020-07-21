c0f97996b44de4cc339143cd5b773771fbc24a0d
mariaverbene <send.to.marya@gmail.com>
2020-07-21 11:37:10 +0300

    second commit

---
+++ src/main/java/kafkaspring/ConfigProperties.java
@@ -0,0 +1,21 @@
+package kafkaspring;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("config")
+public class ConfigProperties {
+    private String topicFrom;
+    private String topicTo;
+    private int numberRecords;
+
+    public String getTopicFrom() { return topicFrom; }
+    public void setTopicFrom(String topicFrom) { this.topicFrom = topicFrom; }
+
+    public String getTopicTo() { return topicTo; }
+    public void setTopicTo(String topicTo) { this.topicTo = topicTo; }
+
+    public void setNumberRecords(int numberRecords) { this.numberRecords = numberRecords; }
+    public int getNumberRecords() { return numberRecords; }
+}
